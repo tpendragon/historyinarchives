1e6b51a437a18d0e7c8197836dd12526a7b1606a
Trey Pendragon <tpendragon@princeton.edu>
2025-10-05 14:47:31 -0700

    Maybe this?

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tpendragon.github.io/historyinarchives',
+  site: 'https://tpendragon.github.io/historyinarchives/',
   integrations: [mdx(), sitemap()],
 
   vite: {
